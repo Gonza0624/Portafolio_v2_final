d33788b74b34bf6802adf7cad879c8e4baedda75
Gonza0624 <gonzalosalazar1180@gmail.com>
2023-04-25 11:08:23 -0500

    tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import About from "./components/about/about";
 import Certificates from "./components/certificates/certificates";
 import ContactForm from "./components/contactForm/contactForm";
@@ -8,8 +9,13 @@ import Objective from "./components/objective/objective";
 import Projects from "./components/projects/projects";
 import MainSkills from "./components/skills/mainSkills";
 import { ThemeProvider } from "./components/themeContext/themeContext";
+import ReactGA from "react-ga";
 
 const App = () => {
+  useEffect(() => {
+    ReactGA.initialize("G-EEPGJ93BDG");
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
     <ThemeProvider>
       <Navigation />
